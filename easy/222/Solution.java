31117cbec621a94ee011d1f02b4cde17abf186df
Heramb Joshi <herambku@gmail.com>
2024-07-01 22:19:31 -0500

    Added solution to problem 222

---
+++ easy/222/Solution.java
@@ -0,0 +1,31 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public int countNodes(TreeNode root) {
+        return counter(root);
+    }
+
+    int counter(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.right == null && root.left == null) {
+            return 1;
+        }
+        var l = counter(root.left);
+        var r = counter(root.right);
+        return 1 + l + r;
+    }
+}
