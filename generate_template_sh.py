9e34895c561ab138ea38937d9f4f46bd9b6349a7
Joshi <hujoshi2@illinois.edu>
2024-07-10 15:25:43 -0500

    created two versions of file template generator

---
+++ generate_template_sh.py
@@ -0,0 +1,21 @@
+# usage: $> generate_template.py problem-level problem-number
+# problem-level : easy medium hard | .
+# problem-number : <problem number on leetcode>
+
+import sys
+import os
+pl = sys.argv[1]
+pn = sys.argv[2]
+
+cwd = '"' + os.getcwd()
+
+if (pl != '.'):
+    if (not os.path.exists(cwd + "\\" +pl + "\"")):
+        os.system("mkdir " + cwd + "\\" +pl + "\"")
+    pl += "\\"
+else:
+    pl = ''
+
+os.system("mkdir " + cwd + "\\" + pl + str(pn) + "\"") # make directory with problem number
+sol_path = cwd + "\\" +pl + str(pn) +"\\"
+os.system("touch " + sol_path + "Solution.java\" " + sol_path + "README.md\"") # create files for specific problem number
